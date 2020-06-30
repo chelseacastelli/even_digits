7a53e59be93a94f1abe529c4a7ad6556128a187d
chelseacastelli <chelsea.castelli@students.makeschool.com>
2020-06-30 19:19:20 -0400

    Complete challenge

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-class Solution:
-    def findNumbers(self, nums: [int]) -> int:
-        pass
+
+def findNumbers(nums) -> int:
+    total_even = 0
+    for n in nums:
+        if len(str(n)) % 2 == 0:
+            total_even += 1
+
+    return total_even
+
+
+if __name__ == '__main__':
+    arr = [12,345,2,6,7896]
+    print(findNumbers(arr))
+
+    arr2 = [555,901,482,1771]
+    print(findNumbers(arr2))
+
